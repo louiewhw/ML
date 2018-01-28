344d0cdd015441f95a953f1e3e142ffe0349833c
Louie <louiewhw@gmail.com>
2018-01-27 16:56:05 -0800

    Add files via upload

---
+++ ML.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 26 10:39:44 2018
+
+@author: louie.wong
+"""
+
+from sklearn.datasets import load_iris
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+
+
+#load datasset
+iris = load_iris()
+
+X = iris.data
+y = iris.target
+#Load data in to a dataframe
+df = pd.DataFrame(data = iris.data, columns = iris.feature_names)
+df['target'] = iris.target
+
+#split training testing
+X_training, X_testing, y_training, y_testing = train_test_split(X, y, test_size = 0.7)
+
+#train Classifier 
+d_tree = tree.DecisionTreeClassifier()
+k_nei = KNeighborsClassifier()
+d_tree.fit(X_training, y_training)
+k_nei.fit(X_training, y_training)
+
+
+#Result
+predictions1 = d_tree.predict(X_testing)
+predictions2 = k_nei.predict(X_testing)
+
+print(accuracy_score(y_testing, predictions1))
+print(accuracy_score(y_testing, predictions2))
