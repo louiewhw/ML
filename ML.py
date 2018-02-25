1c17e9e21c7fec96459e86038043f1f35244734f
louiewhw <louiewhw@gmail.com>
2018-02-25 12:11:46 -0800

    updated

--- ML.py
+++ ML.py
@@ -6,9 +6,12 @@ Created on Fri Jan 26 10:39:44 2018
 """
 
 from sklearn.datasets import load_iris
-from sklearn import tree
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+
 from sklearn.metrics import accuracy_score
 import pandas as pd
 
@@ -27,15 +30,19 @@ df['target'] = iris.target
 X_training, X_testing, y_training, y_testing = train_test_split(X, y, test_size = 0.7)
 
 #train Classifier 
-d_tree = tree.DecisionTreeClassifier()
+d_tree = DecisionTreeClassifier()
 k_nei = KNeighborsClassifier()
+NeuralNet = MLPClassifier()
+
 d_tree.fit(X_training, y_training)
 k_nei.fit(X_training, y_training)
-
+NeuralNet.fit(X_training, y_training)
 
 #Result
 predictions1 = d_tree.predict(X_testing)
 predictions2 = k_nei.predict(X_testing)
+predictions3 = NeuralNet.predict(X_testing)
 
 print(accuracy_score(y_testing, predictions1))
 print(accuracy_score(y_testing, predictions2))
+print(accuracy_score(y_testing, predictions3))
